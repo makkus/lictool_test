c209e4e68043c47fd25a34143268adbf3213a2a4
Markus Binsteiner <markus@frkl.io>
2025-08-12 10:54:02 +0200

    chore: state after ruff & end-of-line pre-commit tasks

--- src/lictool_test/__init__.py
+++ src/lictool_test/__init__.py
@@ -1,22 +1,22 @@
 """
- __init__.py
+__init__.py
 
- Copyright (c) 2025 Markus Binsteiner
- All rights reserved.
+Copyright (c) 2025 Markus Binsteiner
+All rights reserved.
 
- SPDX-License-Identifier: Apache-2.0
+SPDX-License-Identifier: Apache-2.0
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 from typing import IO, Any, Union
@@ -63,7 +63,6 @@ try:
         file: Union[IO[str], None] = None,
         flush: bool = False,
     ) -> None:
-
         # objs = (
         #     ["[green]----------------------------------------------[/green]"]
         #     + list(objects)
